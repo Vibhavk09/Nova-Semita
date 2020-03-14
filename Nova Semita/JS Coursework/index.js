4851f7baf81a0b248db433222b5913d3ceadc67f
Vibhavk09 <43210807+Vibhavk09@users.noreply.github.com>
2020-03-14 20:30:30 +0530

    First commit

---
+++ Nova Semita/JS Coursework/index.js
@@ -0,0 +1,13 @@
+//console.log("Hello World");
+
+let firstName='Vibhav';
+let lastName='Kamat';
+var name=firstName+" "+lastName;
+console.log(name);
+let person={
+    name:'Vibhav',
+    age:21
+};
+console.log(person);
+let selectedColours=["Hello","There",{age:21,dob:12},1];
+console.log(selectedColours);
