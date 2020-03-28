4ca4a01933f0d70dee451418406f0284a00fbaa3
Vibhavk09 <vibhav9998@gmail.com>
2020-03-28 17:06:42 +0530

    Appended in React-Scrimba

--- Nova Semita/React-Scrimba/index.js
+++ Nova Semita/React-Scrimba/index.js
@@ -1,4 +1,41 @@
 import React from "react"
-import ReactDom from "react-dom"
+import ReactDOM from "react-dom"
 
-ReactDom.render(<h1>Hello React</h1>,document.getElementById("root"))
+
+function MyInfo()
+{
+  return (
+    <div>
+      <h1>Vibhav</h1>
+      <p>Hello , this is Vibhav Kamat</p>
+      <ol>
+      <li>Bali</li>
+      <li>Switzerland</li>
+      <li>Phillipines</li>
+      </ol>
+    </div>
+  )
+}
+
+ReactDOM.render(<MyInfo/>,document.getElementById("root"))
+
+
+
+// function MyApp()
+// {
+//     return (
+//         <div>
+//             <ul>
+//                 <li>1
+//                 </li>
+//                 <li>2
+//                 </li>
+//                 <li>3
+//                 </li>
+//             </ul>
+//         </div>
+//     )
+// }
+
+// JSX
+//ReactDOM.render(<ul><li>1</li><li>2</li><li>3</li></ul>, document.getElementById("root"))
